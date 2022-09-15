63d9675f1cbdfe039430ad238b275f13bdd2c4aa
Andre Luis Pereira Martins <aluis@indracompany.com>
2022-09-14 21:36:27 -0300

    refactor: 💡 Axios requests without ssl certificate

--- index.ts
+++ index.ts
@@ -1,19 +1,39 @@
 import axios from 'axios';
 import * as https from 'https';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const call = async () => {
   const httpsAgent = new https.Agent({
     rejectUnauthorized: false,
   });
 
   const { data } = await axios.get(
-    'https://jsonplaceholder.typicode.com/todos',
+    'https://jsonplaceholder.typicode.com/todos/1',
     {
       httpsAgent,
     }
   );
 
-  console.log(data);
+  const todo = data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+  The todo with ID: ${id}
+  Has a title of: ${title}
+  Is it finished: ${finished}
+  `);
+  };
+
+  logTodo(id, title, finished);
 };
 
 call();
